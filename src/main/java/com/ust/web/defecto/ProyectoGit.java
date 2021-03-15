8df20dbb6913a16b3bf220242d7080126ed0a698
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-03-15 10:12:14 +0100

    Comienzo del proyecto

---
+++ src/main/java/com/ust/web/defecto/ProyectoGit.java
@@ -0,0 +1,9 @@
+package com.ust.web.defecto;
+
+public class ProyectoGit {
+
+	int variable1;
+	int variable2;
+	int variable3;
+	
+}
---
+++ src/main/java/com/ust/web/modelo/ModeloGit.java
@@ -0,0 +1,9 @@
+package com.ust.web.modelo;
+
+public class ModeloGit {
+	
+	int variable1;
+	int variable2;
+	int variable3;
+	
+}
